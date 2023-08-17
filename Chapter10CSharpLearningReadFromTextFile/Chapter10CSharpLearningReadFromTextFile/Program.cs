e5562a3611b6e72dd27d3c8712364f16a1e7521f
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-08-17 22:19:26 +0200

    New lesson working with files

---
+++ Chapter10CSharpLearningReadFromTextFile/Chapter10CSharpLearningReadFromTextFile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Chapter10CSharpLearningReadFromTextFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1 - read text
+            string text = System.IO.File.ReadAllText(@"C:\Users\pawel\Downloads\testText.txt");
+            Console.WriteLine("Text contains {0}", text);
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\pawel\Downloads\testText.txt");
+            Console.WriteLine("Content in file is");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\n" + line);
+            }
+
+            // Write text
+            // Method 1
+            string[] linesSave = { "21", "34", "44" };
+            File.WriteAllLines(@"C:\Users\pawel\Downloads\testText2.txt", linesSave);
+            // Method 2
+            Console.WriteLine("Write name");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("Please enter text for the file");
+            string input = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\pawel\Downloads\" + fileName + ".txt", input);
+            // Method 3
+            using(StreamWriter file = new StreamWriter(@"C:\Users\pawel\Downloads\testText3.txt"))
+            {
+                foreach(string line in lines)
+                {
+                    file.WriteLine(line);
+                }
+            }
+        }
+    }
+}
