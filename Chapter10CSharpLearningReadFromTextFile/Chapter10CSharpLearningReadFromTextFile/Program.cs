226d956603284d7b2aa6341998dcecd1a0b5374e
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-08-18 22:22:14 +0200

    Update files to exercise

--- Chapter10CSharpLearningReadFromTextFile/Chapter10CSharpLearningReadFromTextFile/Program.cs
+++ Chapter10CSharpLearningReadFromTextFile/Chapter10CSharpLearningReadFromTextFile/Program.cs
@@ -8,35 +8,51 @@ namespace Chapter10CSharpLearningReadFromTextFile
         static void Main(string[] args)
         {
             // Example 1 - read text
-            string text = System.IO.File.ReadAllText(@"C:\Users\pawel\Downloads\testText.txt");
-            Console.WriteLine("Text contains {0}", text);
+            ////string text = System.IO.File.ReadAllText(@"C:\Users\pawel\Downloads\testText.txt");
+            //Console.WriteLine("Text contains {0}", text);
 
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\pawel\Downloads\testText.txt");
-            Console.WriteLine("Content in file is");
-            foreach (string line in lines)
-            {
-                Console.WriteLine("\n" + line);
-            }
+            ////string[] lines = System.IO.File.ReadAllLines(@"C:\Users\pawel\Downloads\testText.txt");
+            //Console.WriteLine("Content in file is");
+            //foreach (string line in lines)
+            //{
+            //    Console.WriteLine("\n" + line);
+            //}
 
             // Write text
             // Method 1
             string[] linesSave = { "21", "34", "44" };
-            File.WriteAllLines(@"C:\Users\pawel\Downloads\testText2.txt", linesSave);
+            //File.WriteAllLines(@"C:\Users\pawel\Downloads\testText2.txt", linesSave);
             // Method 2
-            Console.WriteLine("Write name");
-            string fileName = Console.ReadLine();
-            Console.WriteLine("Please enter text for the file");
-            string input = Console.ReadLine();
+            //Console.WriteLine("Write name");
+            //string fileName = Console.ReadLine();
+            //Console.WriteLine("Please enter text for the file");
+            //string input = Console.ReadLine();
 
-            File.WriteAllText(@"C:\Users\pawel\Downloads\" + fileName + ".txt", input);
-            // Method 3
-            using(StreamWriter file = new StreamWriter(@"C:\Users\pawel\Downloads\testText3.txt"))
+            ////File.WriteAllText(@"C:\Users\pawel\Downloads\" + fileName + ".txt", input);
+            //// Method 3
+            ////using(StreamWriter file = new StreamWriter(@"C:\Users\pawel\Downloads\testText3.txt"))
+            ////{
+            ////    foreach(string line in lines)
+            ////    {
+            ////        //file.WriteLine(line);
+            ////    }
+            ////}
+
+            // Exercise 
+            string element;
+            string[] textLines = System.IO.File.ReadAllLines(@"C:\Users\pawel\Downloads\input.txt");
+            foreach (string line in textLines)
             {
-                foreach(string line in lines)
+                if (line.Contains("split"))
                 {
-                    file.WriteLine(line);
+                    string[] strings = line.Split();
+                    element = strings[4] + " ";
+                    Console.WriteLine(element);
+                    File.AppendAllText(@"C:\Users\pawel\Downloads\output.txt", element);
                 }
+             
             }
+
         }
     }
 }
