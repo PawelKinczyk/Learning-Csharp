1daa9f4755fe45fee492dbd92cd0ee45f99d5bc2
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-09-19 21:38:33 +0200

    Compleate chapter 18

--- Chapter18CSharpLearningLinqToSQL/Chapter18CSharpLearningLinqToSQL/LinqToSqlDataClasses.designer.cs
+++ Chapter18CSharpLearningLinqToSQL/Chapter18CSharpLearningLinqToSQL/LinqToSqlDataClasses.designer.cs
@@ -36,6 +36,12 @@ namespace Chapter18CSharpLearningLinqToSQL
     partial void InsertStudent(Student instance);
     partial void UpdateStudent(Student instance);
     partial void DeleteStudent(Student instance);
+    partial void InsertLecture(Lecture instance);
+    partial void UpdateLecture(Lecture instance);
+    partial void DeleteLecture(Lecture instance);
+    partial void InsertStudentLecture(StudentLecture instance);
+    partial void UpdateStudentLecture(StudentLecture instance);
+    partial void DeleteStudentLecture(StudentLecture instance);
     #endregion
 		
 		public LinqToSqlDataClassesDataContext() : 
@@ -81,6 +87,22 @@ namespace Chapter18CSharpLearningLinqToSQL
 			get
 			{
 				return this.GetTable<Student>();
+			}
+		}
+		
+		public System.Data.Linq.Table<Lecture> Lectures
+		{
+			get
+			{
+				return this.GetTable<Lecture>();
+			}
+		}
+		
+		public System.Data.Linq.Table<StudentLecture> StudentLectures
+		{
+			get
+			{
+				return this.GetTable<StudentLecture>();
 			}
 		}
 	}
@@ -213,6 +235,8 @@ namespace Chapter18CSharpLearningLinqToSQL
 		
 		private System.Nullable<int> _UniversityId;
 		
+		private EntitySet<StudentLecture> _StudentLectures;
+		
 		private EntityRef<University> _University;
 		
     #region Extensibility Method Definitions
@@ -231,6 +255,7 @@ namespace Chapter18CSharpLearningLinqToSQL
 		
 		public Student()
 		{
+			this._StudentLectures = new EntitySet<StudentLecture>(new Action<StudentLecture>(this.attach_StudentLectures), new Action<StudentLecture>(this.detach_StudentLectures));
 			this._University = default(EntityRef<University>);
 			OnCreated();
 		}
@@ -319,6 +344,19 @@ namespace Chapter18CSharpLearningLinqToSQL
 			}
 		}
 		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="Student_StudentLecture", Storage="_StudentLectures", ThisKey="Id", OtherKey="StudentId")]
+		public EntitySet<StudentLecture> StudentLectures
+		{
+			get
+			{
+				return this._StudentLectures;
+			}
+			set
+			{
+				this._StudentLectures.Assign(value);
+			}
+		}
+		
 		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="University_Student", Storage="_University", ThisKey="UniversityId", OtherKey="Id", IsForeignKey=true, DeleteRule="CASCADE")]
 		public University University
 		{
@@ -349,6 +387,324 @@ namespace Chapter18CSharpLearningLinqToSQL
 						this._UniversityId = default(Nullable<int>);
 					}
 					this.SendPropertyChanged("University");
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		
+		private void attach_StudentLectures(StudentLecture entity)
+		{
+			this.SendPropertyChanging();
+			entity.Student = this;
+		}
+		
+		private void detach_StudentLectures(StudentLecture entity)
+		{
+			this.SendPropertyChanging();
+			entity.Student = null;
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.Lecture")]
+	public partial class Lecture : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _Id;
+		
+		private string _Name;
+		
+		private EntitySet<StudentLecture> _StudentLectures;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnIdChanging(int value);
+    partial void OnIdChanged();
+    partial void OnNameChanging(string value);
+    partial void OnNameChanged();
+    #endregion
+		
+		public Lecture()
+		{
+			this._StudentLectures = new EntitySet<StudentLecture>(new Action<StudentLecture>(this.attach_StudentLectures), new Action<StudentLecture>(this.detach_StudentLectures));
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int Id
+		{
+			get
+			{
+				return this._Id;
+			}
+			set
+			{
+				if ((this._Id != value))
+				{
+					this.OnIdChanging(value);
+					this.SendPropertyChanging();
+					this._Id = value;
+					this.SendPropertyChanged("Id");
+					this.OnIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Name", DbType="NVarChar(50) NOT NULL", CanBeNull=false)]
+		public string Name
+		{
+			get
+			{
+				return this._Name;
+			}
+			set
+			{
+				if ((this._Name != value))
+				{
+					this.OnNameChanging(value);
+					this.SendPropertyChanging();
+					this._Name = value;
+					this.SendPropertyChanged("Name");
+					this.OnNameChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="Lecture_StudentLecture", Storage="_StudentLectures", ThisKey="Id", OtherKey="LectureId")]
+		public EntitySet<StudentLecture> StudentLectures
+		{
+			get
+			{
+				return this._StudentLectures;
+			}
+			set
+			{
+				this._StudentLectures.Assign(value);
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		
+		private void attach_StudentLectures(StudentLecture entity)
+		{
+			this.SendPropertyChanging();
+			entity.Lecture = this;
+		}
+		
+		private void detach_StudentLectures(StudentLecture entity)
+		{
+			this.SendPropertyChanging();
+			entity.Lecture = null;
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.StudentLecture")]
+	public partial class StudentLecture : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _Id;
+		
+		private int _StudentId;
+		
+		private int _LectureId;
+		
+		private EntityRef<Lecture> _Lecture;
+		
+		private EntityRef<Student> _Student;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnIdChanging(int value);
+    partial void OnIdChanged();
+    partial void OnStudentIdChanging(int value);
+    partial void OnStudentIdChanged();
+    partial void OnLectureIdChanging(int value);
+    partial void OnLectureIdChanged();
+    #endregion
+		
+		public StudentLecture()
+		{
+			this._Lecture = default(EntityRef<Lecture>);
+			this._Student = default(EntityRef<Student>);
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int Id
+		{
+			get
+			{
+				return this._Id;
+			}
+			set
+			{
+				if ((this._Id != value))
+				{
+					this.OnIdChanging(value);
+					this.SendPropertyChanging();
+					this._Id = value;
+					this.SendPropertyChanged("Id");
+					this.OnIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_StudentId", DbType="Int NOT NULL")]
+		public int StudentId
+		{
+			get
+			{
+				return this._StudentId;
+			}
+			set
+			{
+				if ((this._StudentId != value))
+				{
+					if (this._Student.HasLoadedOrAssignedValue)
+					{
+						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
+					}
+					this.OnStudentIdChanging(value);
+					this.SendPropertyChanging();
+					this._StudentId = value;
+					this.SendPropertyChanged("StudentId");
+					this.OnStudentIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_LectureId", DbType="Int NOT NULL")]
+		public int LectureId
+		{
+			get
+			{
+				return this._LectureId;
+			}
+			set
+			{
+				if ((this._LectureId != value))
+				{
+					if (this._Lecture.HasLoadedOrAssignedValue)
+					{
+						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
+					}
+					this.OnLectureIdChanging(value);
+					this.SendPropertyChanging();
+					this._LectureId = value;
+					this.SendPropertyChanged("LectureId");
+					this.OnLectureIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="Lecture_StudentLecture", Storage="_Lecture", ThisKey="LectureId", OtherKey="Id", IsForeignKey=true, DeleteOnNull=true, DeleteRule="CASCADE")]
+		public Lecture Lecture
+		{
+			get
+			{
+				return this._Lecture.Entity;
+			}
+			set
+			{
+				Lecture previousValue = this._Lecture.Entity;
+				if (((previousValue != value) 
+							|| (this._Lecture.HasLoadedOrAssignedValue == false)))
+				{
+					this.SendPropertyChanging();
+					if ((previousValue != null))
+					{
+						this._Lecture.Entity = null;
+						previousValue.StudentLectures.Remove(this);
+					}
+					this._Lecture.Entity = value;
+					if ((value != null))
+					{
+						value.StudentLectures.Add(this);
+						this._LectureId = value.Id;
+					}
+					else
+					{
+						this._LectureId = default(int);
+					}
+					this.SendPropertyChanged("Lecture");
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="Student_StudentLecture", Storage="_Student", ThisKey="StudentId", OtherKey="Id", IsForeignKey=true, DeleteOnNull=true, DeleteRule="CASCADE")]
+		public Student Student
+		{
+			get
+			{
+				return this._Student.Entity;
+			}
+			set
+			{
+				Student previousValue = this._Student.Entity;
+				if (((previousValue != value) 
+							|| (this._Student.HasLoadedOrAssignedValue == false)))
+				{
+					this.SendPropertyChanging();
+					if ((previousValue != null))
+					{
+						this._Student.Entity = null;
+						previousValue.StudentLectures.Remove(this);
+					}
+					this._Student.Entity = value;
+					if ((value != null))
+					{
+						value.StudentLectures.Add(this);
+						this._StudentId = value.Id;
+					}
+					else
+					{
+						this._StudentId = default(int);
+					}
+					this.SendPropertyChanged("Student");
 				}
 			}
 		}
--- Chapter18CSharpLearningLinqToSQL/Chapter18CSharpLearningLinqToSQL/MainWindow.xaml.cs
+++ Chapter18CSharpLearningLinqToSQL/Chapter18CSharpLearningLinqToSQL/MainWindow.xaml.cs
@@ -34,13 +34,22 @@ namespace Chapter18CSharpLearningLinqToSQL
             dataContext = new LinqToSqlDataClassesDataContext(connectionString);
 
             //InsertUniversities();
-            InsertStudent();
+            //InsertStudent();
+            //InsertLectures();
+            //InsertStudentLectureAssociations();
+            //GetUniversityOfMate();
+            //GetLecturesOfMate();
+            //GetAllStudentsFromYale();
+            //GetAllUniversitiesWithMales();
+            //GetAllLecturesFromBeijing();
+            //UpdateMat();
+            DeleteRose();
         }
 
         public void InsertUniversities()
         {
             dataContext.ExecuteCommand("delete from University");
-            
+
             University yale = new University();
             yale.Name = "Yale";
             dataContext.Universities.InsertOnSubmit(yale);
@@ -61,14 +70,115 @@ namespace Chapter18CSharpLearningLinqToSQL
 
             List<Student> students = new List<Student>();
 
-            students.Add(new Student { Name= "Carla", Gender = "female", UniversityId = yale.Id });
+            students.Add(new Student { Name = "Carla", Gender = "female", UniversityId = yale.Id });
             students.Add(new Student { Name = "Mat", Gender = "male", University = yale });
             students.Add(new Student { Name = "Rose", Gender = "female", University = beijing });
-            students.Add(new Student { Name = "Mat", Gender = "male", University = beijing });
+            students.Add(new Student { Name = "Mate", Gender = "male", University = beijing });
 
             dataContext.Students.InsertAllOnSubmit(students);
             dataContext.SubmitChanges();
 
+            MainDataGrid.ItemsSource = dataContext.Students;
+        }
+
+        public void InsertLectures()
+        {
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "Math" });
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "History" });
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Lectures;
+        }
+
+        public void InsertStudentLectureAssociations()
+        {
+            Student Carla = dataContext.Students.First(st => st.Name.Equals("Carla"));
+            Student Mat = dataContext.Students.First(st => st.Name.Equals("Mat"));
+            Student Rose = dataContext.Students.First(st => st.Name.Equals("Rose"));
+            Student Mate = dataContext.Students.First(st => st.Name.Equals("Mate"));
+
+            Lecture Math = dataContext.Lectures.First(lc => lc.Name.Equals("Math"));
+            Lecture History = dataContext.Lectures.First(lc => lc.Name.Equals("History"));
+
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Carla, Lecture = Math });
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Mat, Lecture = History });
+
+
+            StudentLecture slMat = new StudentLecture();
+            slMat.Student = Mat;
+            slMat.LectureId = History.Id;
+            dataContext.StudentLectures.InsertOnSubmit(slMat);
+
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Rose, Lecture = History });
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Mate, Lecture = Math });
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.StudentLectures;
+        }
+
+        public void GetUniversityOfMate()
+        {
+            Student Mate = dataContext.Students.First(st => st.Name.Equals("Mate"));
+
+            University MateUniversity = Mate.University;
+            List<University> universities = new List<University>();
+            universities.Add(MateUniversity);
+
+            MainDataGrid.ItemsSource = universities;
+        }
+
+        public void GetLecturesOfMate()
+        {
+            Student Mate = dataContext.Students.First(st => st.Name.Equals("Mate"));
+
+            var mateLectures = from sl in Mate.StudentLectures select sl.Lecture;
+
+            MainDataGrid.ItemsSource = mateLectures;
+        }
+
+        public void GetAllStudentsFromYale()
+        {
+            var studentsFromYale = from student in dataContext.Students where student.University.Name == "Yale" select student;
+
+            MainDataGrid.ItemsSource = studentsFromYale;
+        }
+
+        public void GetAllUniversitiesWithMales()
+        {
+            var malesUniversities = from student in dataContext.Students
+                                    join university in dataContext.Universities
+                                    on student.University equals university
+                                    where student.Gender == "male"
+                                    select university;
+
+            MainDataGrid.ItemsSource = malesUniversities;
+        }
+
+        public void GetAllLecturesFromBeijing()
+        {
+            var lecturesFromBeijing = from sl in dataContext.StudentLectures
+                                      join student in dataContext.Students
+                                      on sl.StudentId equals student.Id
+                                      where student.University.Name == "Beijing"
+                                      select sl.Lecture;
+            MainDataGrid.ItemsSource = lecturesFromBeijing;
+        }
+
+        public void UpdateMat()
+        {
+            Student Mate = dataContext.Students.FirstOrDefault(st => st.Name == "Mat");
+            Mate.Name = "Antonio";
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.Students;
+        }
+
+        public void DeleteRose()
+        {
+            Student Rose = dataContext.Students.FirstOrDefault(st => st.Name == "Rose");
+            dataContext.Students.DeleteOnSubmit(Rose);
+            dataContext.SubmitChanges();
             MainDataGrid.ItemsSource = dataContext.Students;
         }
 
