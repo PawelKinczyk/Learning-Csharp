79d8b114f8b7610031e19c82c0bb2217bb2c8b08
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-09-19 22:01:03 +0200

    Learning about threads

---
+++ Chapter21CSharpLearningThreadsBasics/Chapter21CSharpLearningThreadsBasics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter21CSharpLearningThreadsBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World 1!");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World 2!");
+            //Console.WriteLine("Hello World 3!");
+            //Console.WriteLine("Hello World 4!");
+
+            new Thread(() => 
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 3");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 4");
+            }).Start();
+        }
+    }
+}
