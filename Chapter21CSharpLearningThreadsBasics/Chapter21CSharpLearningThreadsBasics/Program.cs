4b227ecd9fdf6936eeecd4d0ac0edd4b5130b236
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-09-20 20:53:14 +0200

    Learning about threads

--- Chapter21CSharpLearningThreadsBasics/Chapter21CSharpLearningThreadsBasics/Program.cs
+++ Chapter21CSharpLearningThreadsBasics/Chapter21CSharpLearningThreadsBasics/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Chapter21CSharpLearningThreadsBasics
 {
@@ -13,24 +15,40 @@ namespace Chapter21CSharpLearningThreadsBasics
             //Console.WriteLine("Hello World 3!");
             //Console.WriteLine("Hello World 4!");
 
-            new Thread(() => 
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 1");
-            }).Start();
+            //new Thread(() => 
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 1");
+            //}).Start();
 
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 2");
-            }).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 2");
+            //}).Start();
 
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 3");
-            }).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 3");
+            //}).Start();
 
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 4");
+            //}).Start();
+
+            Enumerable.Range(0, 100).ToList().ForEach(f =>
+            {
+                new Thread(() =>
+                {
+                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                    Thread.Sleep(1000);
+                }).Start();
+            });
+
+            Console.ReadLine();
         }
     }
 }
