a46afe417d4241ceb66855829a0b41e8d4a2dcf2
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-09-20 22:04:27 +0200

    Learn about threads on project

---
+++ Chapter21CSharpLearningTaskThreads/Chapter21CSharpLearningTaskThreads/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter21CSharpLearningTaskThreads
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached("Html", typeof(string), typeof(MainWindow), new FrameworkPropertyMetadata(OnHtmlChanged));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                Debug.WriteLine($"Thread nr. {Thread.CurrentThread.ManagedThreadId}");
+                HttpClient webClient = new HttpClient();
+                string html = webClient.GetStringAsync("https://google.com").Result;
+                MyButton.Dispatcher.Invoke(() =>
+                {
+                    Debug.WriteLine($"Thread nr. {Thread.CurrentThread.ManagedThreadId}");
+                    MyButton.Content = "Done";
+                });
+                
+            });
+
+        }
+
+        private async void MyButton_Click2(object sender, RoutedEventArgs e)
+        {
+            string myHtml = "Bla";
+
+            Debug.WriteLine($"Thread before wait {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Run(async () =>
+            {
+                Debug.WriteLine($"Thread nr. {Thread.CurrentThread.ManagedThreadId}");
+                HttpClient webClient = new HttpClient();
+                string html = webClient.GetStringAsync("https://google.com").Result;
+                myHtml = html;
+
+            });
+            Debug.WriteLine($"Thread after wait {Thread.CurrentThread.ManagedThreadId}");
+            MyButton.Content = "Downloading";
+            MyWebBrowser.SetValue(HtmlProperty, myHtml);
+        }
+
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser webBrowser = dependencyObject as WebBrowser;
+            if (webBrowser != null) 
+                webBrowser.NavigateToString(e.NewValue as string);
+        }
+    }
+}
